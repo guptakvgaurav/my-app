0f2e0b885190bfae6a5f499c09a068c4bf23e7bf
guptakvgaurav <guptakvgaurav@gmail.com>
2018-12-14 18:15:59 +0530

    Added sweetalert

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import LoginCard from '../../components/login_card';
+import SweetAlert from 'react-bootstrap-sweetalert';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      message: null,
+      showModal: false
+    }
+  }
+  
+  showModal = (status) => {
+    this.setState({
+      showModal: status
+    })
+  }
+
+  onLogin = (data) => {
+    // TODO: data should be { username: <>, password: <>}
+    this.showModal(true);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <LoginCard />
+          <SweetAlert success="success"
+                      show={ this.state.showModal}
+                      title="Success!" 
+                      onConfirm={() => this.showModal(false)}>
+            This is just to notify that you have clicked login button.
+          </SweetAlert>
+          <LoginCard onSubmit={ this.onLogin }/>
         </header>
+        <h1>{ this.state.message } </h1>
       </div>
     );
   }
